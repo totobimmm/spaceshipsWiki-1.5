67549149b3e196bb04671d90b0b942221e1870a0
totobimmm <theospini@gmail.com>
2024-06-28 15:21:47 +0200

    su

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Suspense } from "react";
-import Loading from "./components/Loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
